a9aecb6882138078c89154b4d39cfeb29cfbc307
centrex <wellcom74@gmail.com>
2017-05-19 14:49:27 +0300

    Перерисовал форму. Осталось добавить листнеры и связать с парсером и базой.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -12,68 +12,79 @@ public class MainWindow extends JFrame{
     public MainWindow(){
         setTitle("Dream Interpretation");
         setIconImage(new ImageIcon("animal-paw-print.png").getImage()); //добавить иконку в заголовок окна
-        setSize(800, 500);
+        setSize(800, 800);
         setLocationRelativeTo(null);
+        setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 
     //Создаем пару панелей
-        setLayout(null);
-        JPanel TextFieldsPane = new JPanel();
-        TextFieldsPane.setBounds(0,0,700,300);
-        TextFieldsPane.setBackground(new Color(100, 200, 50));
+        JPanel textFieldsPane = new JPanel();
+        textFieldsPane.setBackground(new Color(100, 200, 50));
+        add(textFieldsPane, BorderLayout.CENTER);
 
         JPanel ButtonPane = new JPanel();
-        ButtonPane.setBounds(0,700,800,100);
         ButtonPane.setBackground(new Color(100, 100, 70));
-        add(TextFieldsPane);
-        add(ButtonPane);
+        add(ButtonPane, BorderLayout.SOUTH);
 
     //Добавляем кнопку на нижнюю панель
-        ButtonPane.setLayout(null);
-        JButton jbuttonAnalyze = new JButton("Waiting for Meaning");
-        jbuttonAnalyze.setBackground(new Color(50, 100, 200));
+        //ButtonPane.setLayout(null);
+        JButton jbuttonAnalyze = new JButton("Request for Interpretation");
+        jbuttonAnalyze.setBackground(new Color(160, 200, 200));
         jbuttonAnalyze.setBounds(350, 725,100,50);
-        add(jbuttonAnalyze);
+        ButtonPane.add(jbuttonAnalyze);
 
     //Добавляем текстовые поля на верхню панель
-        TextFieldsPane.setLayout(new GridLayout(10,1));
-
-        //реализовать как массив, чтобы легче писать в базу и искать?
-        JTextField Name = new JTextField("Ваше Имя: ", 50);
-        TextFieldsPane.add(Name);
-
-        JTextField Age = new JTextField("Возраст: ", 20);
-        TextFieldsPane.add(Age);
-
-        JTextField Occupation = new JTextField("Сфера деятельности: ", 100);
-        TextFieldsPane.add(Occupation);
+        //TextFieldsPane.setLayout(new GridLayout(10,2));
 
         String[] MartialStatusCombo = {"Женат/Замужем", "Разведен(а)", "В разводе", "Вдовец/Вдова"};
-        JComboBox<String> jcombo1 = new JComboBox<String>(MartialStatusCombo);
-        TextFieldsPane.add(jcombo1);
-        jcombo1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println(jcombo1.getSelectedItem().toString());
-            }
-        });
+        JComboBox<String> jcombo1 = new JComboBox<>(MartialStatusCombo);
+        jcombo1.setPreferredSize(new Dimension(750,20));
+        textFieldsPane.add(jcombo1);
+        jcombo1.addActionListener(e -> System.out.println(jcombo1.getSelectedItem().toString()));
 
-        JTextArea EventsBeforeDream = new JTextArea("Значимые события перед сновидением");
-        JScrollPane EventsBeforeDreamScroll = new JScrollPane(EventsBeforeDream);
-        TextFieldsPane.add(EventsBeforeDream);
+        JLabel lName = new JLabel("Ваше Имя: ");
+        textFieldsPane.add(lName);
+        JTextField jName = new JTextField( 60);
+        textFieldsPane.add(jName);
 
-        JTextArea ThoughtsAboutDream = new JTextArea("Мысли по поводу сна - на что это похоже");
-        JScrollPane ThoughtsAboutDreamScroll = new JScrollPane(ThoughtsAboutDream);
-        TextFieldsPane.add(ThoughtsAboutDream);
+        JLabel lAge = new JLabel("Возраст:     ");
+        textFieldsPane.add(lAge);
+        JTextField Age = new JTextField(60);
+        textFieldsPane.add(Age);
 
-        JTextArea FeelingsAboutDream = new JTextArea("Чувства, вызванные сном");
-        JScrollPane FeelingsAboutDreamScroll = new JScrollPane(FeelingsAboutDream);
-        TextFieldsPane.add(FeelingsAboutDream);
+        JLabel lOccupation = new JLabel("Сфера деятельности: ");
+        textFieldsPane.add(lOccupation);
+        JTextField occupation = new JTextField(55);
+        textFieldsPane.add(occupation);
 
-        JTextArea Dream = new JTextArea("Содержание сна");
-        JScrollPane DreamScroll = new JScrollPane(Dream);
-        TextFieldsPane.add(FeelingsAboutDream);
+        JLabel lEvents = new JLabel("Значимые события перед сновидением:");
+        textFieldsPane.add(lEvents);
+        JTextArea eventsBeforeDream = new JTextArea(7, 40);
+        eventsBeforeDream.setLineWrap(true);
+        JScrollPane eventsBeforeDreamScroll = new JScrollPane(eventsBeforeDream);
+        textFieldsPane.add(eventsBeforeDreamScroll);
+
+        JLabel lThoughts = new JLabel("Мысли по поводу сна (На что это похоже?):");
+        textFieldsPane.add(lThoughts);
+        JTextArea thoughtsAboutDream = new JTextArea(7, 40);
+        thoughtsAboutDream.setLineWrap(true);
+        JScrollPane thoughtsAboutDreamScroll = new JScrollPane(thoughtsAboutDream);
+        textFieldsPane.add(thoughtsAboutDreamScroll);
+
+        JLabel lFellings = new JLabel("Чувства, вызванные сном:");
+        textFieldsPane.add(lFellings);
+        JTextArea feelingsAboutDream = new JTextArea(7, 50);
+        feelingsAboutDream.setLineWrap(true);
+        JScrollPane feelingsAboutDreamScroll = new JScrollPane(feelingsAboutDream);
+        textFieldsPane.add(feelingsAboutDreamScroll);
+
+        JLabel lDream = new JLabel("Содержание сна:");
+        textFieldsPane.add(lDream);
+        JTextArea dream = new JTextArea(7, 60);
+        dream.setLineWrap(true);
+        JScrollPane dreamScroll = new JScrollPane(dream);
+        textFieldsPane.add(dreamScroll);
 
         setVisible(true);
     }
