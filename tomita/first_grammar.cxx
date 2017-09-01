e70e68d9276b9db7c4a8f30de2061d842231e295
Aleksandr Afonin <aafonin@octspb.ru>
2017-09-01 13:27:20 +0300

    Добавил папку с парсером и словарь психоаналитических терминов

---
+++ tomita/first_grammar.cxx
@@ -0,0 +1,4 @@
+﻿#encoding "utf8"
+
+PP ->  "в" Noun interp (Fact.Field1);
+S -> Verb PP;
---
+++ tomita/first_grammar_1.cxx
@@ -0,0 +1,4 @@
+﻿#encoding "utf8"
+
+PP -> "в" Noun interp (Fact.Field1); //меняем терминал Prep на лемму "в" и выводим (интерпретируем) существительное
+S -> Verb PP;
